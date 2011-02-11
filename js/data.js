637ff54f5b0a3b407c08face1cea7328cd21c1dc
Denis Kokin <denis.kokin@parcsis.org>
2011-02-11 14:18:07 +0300

    Data for popup added

--- js/data.js
+++ js/data.js
@@ -1,3 +1,22 @@
 var data = [
-  
+  1: {
+    url: 'http://www.membrana.ru/particle/15709',
+    tags: ['география', 'архитектура', 'полный бред', 'бугогашечки']
+  },
+  2: {
+    url: 'http://www.membrana.ru/particle/15708',
+    tags: ['электроника', 'гейство', 'ололо']
+  },
+  3: {
+    url: 'http://www.membrana.ru/particle/15707',
+    tags: ['электроника', 'наноняшечки']
+  },
+  4: {
+    url: 'http://www.membrana.ru/particle/15706',
+    tags: ['компьютеры', 'мелкота', 'невероятное рядом']
+  },
+  5: {
+    url: 'http://www.membrana.ru/particle/15705',
+    tags: ['маленькие засранцы', 'мелкота', 'ололо', 'как страншно жить', 'биология']
+  }
 ];
